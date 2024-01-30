619446784a52ca4cfa7c40a304a8a7814fca9e9b
Dermot Bateman <dermotbateman@gmail.com>
2024-01-30 11:27:33 +0200

    done: 8.1

--- library-backend.js
+++ library-backend.js
@@ -98,14 +98,32 @@ let books = [
 */
 
 const typeDefs = `
-  type Query {
-    dummy: Int
-  }
+
+type Book {
+  title: String!
+  published: Int!
+  author: String!
+  id: ID
+  genres: [String!]!
+}
+
+type Author {
+  name: String!
+  id: ID
+  born: Int!
+}
+
+type Query {
+  bookCount: Int!
+  authorCount: Int!
+}
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0
+    bookCount: () => books.length,
+    authorCount: () => authors.length
+
   }
 }
 
